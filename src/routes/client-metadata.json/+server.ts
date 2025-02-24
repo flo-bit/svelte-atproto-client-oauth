6b551cdf90bebef431f153e5fdc5313601b9714f
Florian <45694132+flo-bit@users.noreply.github.com>
2025-02-24 12:40:39 +0100

    prerender

--- src/routes/client-metadata.json/+server.ts
+++ src/routes/client-metadata.json/+server.ts
@@ -1,6 +1,8 @@
 import { metadata } from '$lib/client-metadata';
 import { json } from '@sveltejs/kit';
 
+export const prerender = true;
+
 export async function GET() {
 	return json(metadata);
 }
